5c7e2371b2b678f4109085be9f79132873b649e1
cheeknaew <cheeknaew@gmail.com>
2017-01-19 16:32:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -152,7 +152,40 @@ echo "<br>";
 echo $comment;
 echo "<br>";
 echo $gender;
-?>
+$access_token = '7+wBpEvBpTGZvspYENGA1vUXu80YJ0gPpkktFWd1WjdNbL3KRD9V7cjNNdwxF9NH+y4HXVskptlWs75fu9mwe3BU8+NP1W8csdSUImvH+BEm6Qp3gkwfqMs6UpJCCpbyFaYPCfof1dHsmuH3gk/2pQdB04t89/1O/w1cDnyilFU=';
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			
+			$messages = [
+					'type' => 'text',
+					'text' => "Earth เป็นคนหล่อมากๆ"
+				];
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+}
 
 </body>
 </html>
